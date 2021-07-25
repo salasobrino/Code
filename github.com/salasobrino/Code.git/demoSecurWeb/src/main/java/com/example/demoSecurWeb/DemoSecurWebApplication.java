b90acd291fb564a572350b748b2f744048e60c68
salas <salas@DESKTOP-O1IRU82>
2021-07-25 10:23:09 +0200

    go on

--- github.com/salasobrino/Code.git/demoSecurWeb/src/main/java/com/example/demoSecurWeb/DemoSecurWebApplication.java
+++ github.com/salasobrino/Code.git/demoSecurWeb/src/main/java/com/example/demoSecurWeb/DemoSecurWebApplication.java
@@ -3,6 +3,8 @@ package com.example.demoSecurWeb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//http://www.profesor-p.com/2018/10/17/seguridad-web-en-spring-boot/
+
 @SpringBootApplication
 public class DemoSecurWebApplication {
 
---
+++ github.com/salasobrino/Code.git/demoSecurWeb/src/main/java/implementation/WebController.java
@@ -0,0 +1,34 @@
+package implementation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+	
+	@RequestMapping({"/", "index"})
+	public  String inicio() {
+		return "index";
+	}
+	
+	@RequestMapping("/webprivado")
+	public String privado() {
+		return "privado";
+	}
+	
+	@RequestMapping("/webpublico")
+	public String loginpub() {
+		return "publico";
+	}
+	
+	@RequestMapping("/webadmin")
+	  public String admin() {
+	    return "admin";
+	  }
+	
+	@RequestMapping("/login")
+	  public String login() {
+	    return "login";
+	  }
+
+}
